84fddf91ade348ace8e098c9a4804391b2d50503
Junaid Shah <junaid.zealot20@gmail.com>
2021-09-15 23:26:45 +1000

    update sleep time

--- usage.sh
+++ usage.sh
@@ -6,5 +6,5 @@ CONTAINER=$(docker inspect --format="{{.Id}}" nginx-local)
 # while the container is running, output the stats.
 while [ "$(docker inspect -f {{.State.Running}} $CONTAINER 2>/dev/null)" = "true" ]; do
     docker stats $container --no-stream
-    sleep 5
+    sleep 10
 done
