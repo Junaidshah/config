83d3745284d422ccb022677e1274123983a72025
Junaid Shah <junaid.zealot20@gmail.com>
2021-09-15 23:19:23 +1000

    fix typo

--- word_crawler.py
+++ word_crawler.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from collections import Counter
 
-url = requests.get("http://localhost")
+url = requests.get("http://localhost/")
 
 soup = BeautifulSoup(url.content, features="html.parser")
 
